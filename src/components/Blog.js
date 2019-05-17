96e145bc19b208f46edb3b01e83031e98b8af0b5
Minna Sarakontu <minna.sarakontu@gmail.com>
2019-05-17 19:53:12 +0300

    5.6 added togglable blog info

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,22 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [fullInfo, setFullInfo] = useState(false)
+
+  const showWhenVisible = { display: fullInfo ? '' : 'none' }
+
+  const toggleFullInfo = () => setFullInfo(!fullInfo)
+
+  return (
+    <div class="blog-row" onClick={toggleFullInfo}>
+      <div>{blog.title} ({blog.author})</div>
+      <div style={showWhenVisible}>
+        <div><a href={blog.url}>{blog.url}</a></div>
+        <div>{blog.likes} likes</div>
+        {blog.user && <div>added by {blog.user.name}</div>}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
